386640447555a67e7c91c2fcc2fb84a90c4555bd
GAssalin <gustavo.assalin@hotmail.com>
2016-09-28 10:36:38 -0300

    Criacao da classe modelo pessoa

---
+++ src/java/example/model/Person.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author gustavosa
+ */
+@Entity
+@SequenceGenerator(name = "P_SEQ", sequenceName = "PERSON_SEQ", initialValue = 1, allocationSize = 1)
+public class Person implements Serializable {
+    private static final long serialVersionUID = -4463434212023223185L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "P_SEQ")
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+    private String email;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the birthday
+     */
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * @param birthday the birthday to set
+     */
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
